6b09af121a3201f0fe53532828edfa955f0263fa
Robin Cussol <me@robincussol.com>
2023-03-12 21:31:07 +0000

    fix: use . for CopyPlugin's to field
    Otherwise, we would get nested dist's folders.

--- rspack.config.js
+++ rspack.config.js
@@ -17,11 +17,12 @@ module.exports = {
     new CopyPlugin([
       {
         from: "public",
-        to: "dist",
+        to: ".",
       },
     ]),
   ], // Copy the files from the public directory to the dist directory, aligning the behavior of the CRA
+  // This is Codesandbox-specific
   devServer: {
-    allowedHosts: ["kyq570-8081.csb.app"], // <  Copy and paste the domain from the URL Codebox provides for your runtime here
+    allowedHosts: ["PREVIEW_SUBDOMAIN.csb.app"], // <  Copy and paste the domain from the URL Codebox provides for your runtime here
   },
 };
